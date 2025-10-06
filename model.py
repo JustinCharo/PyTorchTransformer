26b749b8c74173daada65c21740c829882d69ba2
JustinCharo <jbcharo@gmail.com>
2025-10-06 01:54:32 -0500

    Added multihead attention block

--- model.py
+++ model.py
@@ -44,3 +44,77 @@ class PositionalEncoding(nn.Module):
         x = x + (self.PE[:, :x.shape[1], :]).requires_grad_(False)
         return self.dropout(x)
 
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 10**-6):
+        suoer().__init__()
+        # small value to avoid division by zero
+        self.eps = eps
+        # multiplier to scale the normalized output
+        self.alpha = nn.Parameter(torch.ones(1))
+        # added bias to the normalized output
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout: float):
+        super().__init__()
+        self.droupout = nn.Dropout(dropout)
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+        
+    def forward(self, x):
+        # tensor of shape (batch_size, Seq_len, d_model)
+        # maps the input tensor through linear_1 to another tensor of higher dimension of shape (batch_size, seq_len, d_ff)
+        # apply linear 2 which will map the tensor back to the original dimension of shape (batch_size, seq_len, d_model)
+        # apply dropout to the output of linear_2
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+class MultiHeadAttentionBlock(nn.Module):
+    
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisible by h"
+        
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.w_o = nn.Linear(d_model, d_model)
+        self.droupout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (batch_size, seq_len, d_model)
+        key = self.w_k(k) # (batch_size, seq_len, d_model)
+        value = self.w_v(v) # (batch_size, seq_len, d_model)
+
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).tranpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).tranpose(1, 2)
+
+        x, self.attention_scores = self.MultiHeadAttentionBlock(query, key, value, mask, self.dropout)
+
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h + self.d_k)
+
+        return self.w_o(x)
+
